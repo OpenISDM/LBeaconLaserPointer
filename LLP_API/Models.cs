c17acc13587c5558494ed920e383d6ff48c2ee13
唐振嚴 <kenneth@gm.nssh.ntpc.edu.tw>
2018-05-28 15:08:39 +0800

    Add connect web api tool

---
+++ LLP_API/Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LLP_API
+{
+    public partial class BeaconInformation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Floor { get; set; }
+    }
+
+    public partial class LaserPointerInformation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double FaceLongitude { get; set; }
+        public double FaceLatitude { get; set; }
+        public int Floor { get; set; }
+    }
+}
